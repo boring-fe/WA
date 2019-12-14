69799e4227803baa973865cbaeb5adbe6006e9f4
Anatolii K <tol890@ukr.net>
2019-12-14 22:47:30 +0200

    Traffic lights (without commented code)

--- src/lesson_12_1/lesson_12_1.js
+++ src/lesson_12_1/lesson_12_1.js
@@ -3,10 +3,6 @@ import "./lesson_12_1.scss";
 const controlButton = document.querySelector(".traffic-lights__control-button");
 
 const red_lamp = document.getElementById("red_lamp");
-// console.log(red_lamp);
-// red_lamp.onclick = function() {
-//   alert("asdfasd");
-// };
 
 controlButton.onclick = function() {
   console.log("asdasdas");
@@ -15,10 +11,6 @@ const lightButtons = document.querySelectorAll(".traffic-lights__lamp");
 const redButton = document.querySelector(".traffic-lights__lamp_red");
 const yellowButton = document.querySelector(".traffic-lights__lamp_yellow");
 const greenButton = document.querySelector(".traffic-lights__lamp_green");
-
-// function clicked() {
-//   red_lamp.style.background = "rgba(242, 38, 19, 1)";
-// }
 
 function clearColors() {
   redButton.style.backgroundColor = "rgb(39, 34, 34)";
@@ -30,9 +22,6 @@ redButton.setAttribute("data-is-active", true);
 yellowButton.setAttribute("data-is-active", true);
 greenButton.setAttribute("data-is-active", true);
 console.log(redButton.getAttribute("data-is-active"));
-// function checkAttribute(button) {
-
-// }
 
 redButton.onclick = function() {
   if (redButton.getAttribute("data-is-active")) {
@@ -58,10 +47,6 @@ greenButton.onclick = function() {
 controlButton.onclick = function() {
   clearColors();
   redButton.removeAttribute("data-is-active", "false");
-  //   console.log(redButton.getAttribute("data-is-active"));
-
   yellowButton.removeAttribute("data-is-active", "false");
-  //   console.log(yellowButton.getAttribute("data-is-active"));
-
   greenButton.removeAttribute("data-is-active", "false");
 };
